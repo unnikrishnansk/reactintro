1e0fdaca25182c6fe117aee52296b88d80fa5575
unnikrishnansk <unni.sk94@gmail.com>
2022-05-20 20:59:24 +0530

    ReactIntro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
 function App() {
+  
+  let Data1 = [
+    {name : "Android"},
+    {name : "Blakcberry"},
+    {name : "iPhone"},
+    {name : "Windows Phone"}   
+];
+
+let Data2 = [
+    {item : "Samsung"},
+    {item : "HTC"},
+    {item : "Micromax"},
+    {item : "Apple"}
+];
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h2>Mobile Operating System</h2>
+     {Data1.map((e) => (
+       <ul>
+       <div className='notes'>
+         <li>{e.name}</li>
+       </div>
+       </ul>
+     ))}
+
+      <h2>Mobile Manufacturers</h2>
+    {Data2.map((e) => (
+      <ul>
+       <div className='notes'>
+         <li>{e.item}</li>
+       </div>
+       </ul>
+     ))}
+    
     </div>
   );
 }
